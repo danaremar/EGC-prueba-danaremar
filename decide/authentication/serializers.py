e3f623c55a94c6bbd3c4cdc6d402d0a5deed6b40
danaremar <danaremar@alum.us.es>
2021-02-09 18:25:08 +0000

    Bugfix solved

--- decide/authentication/serializers.py
+++ decide/authentication/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from django.contrib.auth.models import User
 
-
+# this is a upload
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
